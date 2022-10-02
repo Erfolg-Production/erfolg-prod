3939df93698f36ffc789a7701bf305fbb56faa28
hunter87ff <dadsamir15@gmail.com>
2022-10-02 18:47:31 +0530

    updated sources

---
+++ docs/static/js/base.js
@@ -0,0 +1,15 @@
+var menu_bar = document.getElementById("menubar");
+count = 0
+
+
+
+function menu_toggle(){
+    if(count == 0){
+        menu_bar.style.visibility = "visible";
+        count = 1
+    }else{
+        menu_bar.style.visibility = "hidden";
+        count = 0
+    }
+}
+
